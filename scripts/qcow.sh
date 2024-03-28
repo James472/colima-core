24fe6cb1e379841de27490d8821da51e32d2e779
dev <dev@mail.cool>
2024-03-28 13:03:28 +0800

    skip upgrade

--- scripts/qcow.sh
+++ scripts/qcow.sh
@@ -67,7 +67,7 @@ install_packages() (
 
     # chroot_exec apt-get autoremove -y
     chroot_exec apt-mark hold linux-image-virtual docker-ce docker-ce-cli containerd.io
-    chroot_exec apt-get upgrade -y
+    # chroot_exec apt-get upgrade -y
     chroot_exec apt-get clean -y
     chroot_exec sh -c "rm -rf /var/lib/apt/lists/* /var/cache/apt/*"
 
